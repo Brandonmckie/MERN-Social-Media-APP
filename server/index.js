44ba24b76bf329fb75371cd329be327729190e16
zippiespick <zippiespick@gmail.com>
2023-03-22 18:59:22 -0400

    finished

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import User from "./models/Users.js"
-import Post from 
+import Post from "./models/Post.js"
 // Configuration
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
