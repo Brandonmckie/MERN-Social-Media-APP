a41524d93803a0435947ca2f220456b77d02f6da
David Jover <david.jover59@gmail.com>
2023-03-22 09:04:04 -0400

    Added UserWidget. Not finished just yet. Imported everything I needed though

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -2,9 +2,9 @@ import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
 const WidgetWrapper = styled(Box)(({ theme }) => ({
-  padding: "1.5rem 1.5rem 0.75rem 1.5rem",
+  padding: "2rem 1.5rem 1rem 2rem",
   backgroundColor: theme.palette.background.alt,
-  borderRadius: "0.75rem",
+  borderRadius: "0.9rem",
 }));
 
 export default WidgetWrapper;
