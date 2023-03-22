b5ad2efc6f66a49d2b312987c01eb631dfe56096
David Jover <david.jover59@gmail.com>
2023-03-22 09:38:50 -0400

    Fixed silly bug caused by improper syntax within typography sections

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -32,7 +32,7 @@ const userWidget = ({ userId, picturePath }) => {
 
   useEffect(() => {
     getUser();
-  }, [])
+  }, []);
 
   if (!user) {
     return null;
@@ -48,92 +48,94 @@ const userWidget = ({ userId, picturePath }) => {
     friends,
   } = user;
 
-  return(
+  return (
     <WidgetWrapper>
-        {/*first row*/}
-        <FlexBetween
-            gap="0.4rem"
-            pb="1rem"
-            onClick={() => navigate(`/profile/${userId}`)}>
-                <FlexBetween gap=".9rem">
-                    <UserImage image={picturePath} />
-                    <Box>
-                        <Typography
-                            variant="h4"
-                            color={dark}
-                            fontWeight="500"
-                            sx={{
-                                "&:hover":{
-                                    color:palette.primary.light,
-                                    cursor: "pointer"
-                                }
-                            }}>
-                                {firstName} {lastName}
-                        </Typography>
-                        <Typography color={medium}> {friends.length} friends</Typography>
-                    </Box>
-                    <ManageAccountsOutlined />
-                </FlexBetween>
-                <Divider />
-                    <Box p=".9rem 0">
-                        <Box display="flex" alignItems="center" gap="1rem" mb="0.4rem">
-                            <LocationOutlined fontSize="large" sx={{ color: main}} />
-                            <Typography color={medium}>{location}</Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" gap="1rem">
-                            <WorkOutlineOutlined fonstize="large" sx={{ color: main }} />
-                            <Typography color={medium}>{occupation}</Typography>
-                        </Box>
-                    </Box>
-                    {/*third row */}
-                    <Box p="1rem 0">
-                        <FlexBetween mb="0.5rem">
-                            <Typography color={medium}>Who's checking you out?</Typography>
-                            <Typography color {main} fontWeight="500">
-                                {viewedProfile}
-                            </Typography>
-                        </FlexBetween>
-                        <FlexBetween mb="0.5rem">
-                            <Typography color={medium}>Content Reactions</Typography>
-                            <Typography color {main} fontWeight="500">
-                                {impressions}
-                            </Typography>
-                        </FlexBetween>
-                    </Box>
-                    <Box p="0.9rem 0">
-                        <Typography fonstize="1rem" color={main} fontWeight="500" mb="1rem">
-                            Community Profiles
-                        </Typography>
-                        <FlexBetween gap="1rem" mb="0.5rem">
-                            <FlexBetween gap="1rem">
-                                <img src="../assets/twitter.png" alt="twitter" />
-                                <Box>
-                                    <Typography color {main} fontWeight="500">
-                                        Twitter
-                                    </Typography>
-                                    <Typography color {medium}>Your Network</Typography>
-                                </Box>
-                            </FlexBetween>
-                            <EditOutlined sx={{ color: main }} />
-                        </FlexBetween>
-
-                        <Typography fonstize="1rem" color={main} fontWeight="500" mb="1rem">
-                            Community Profiles
-                        </Typography>
-                        <FlexBetween gap="1rem" mb="0.5rem">
-                            <FlexBetween gap="1rem">
-                                <img src="../assets/linkedIn.png" alt="linkedIn" />
-                                <Box>
-                                    <Typography color {main} fontWeight="500">
-                                        linkedIn
-                                    </Typography>
-                                    <Typography color {medium}>Network App</Typography>
-                                </Box>
-                            </FlexBetween>
-                            <EditOutlined sx={{ color: main }} />
-                        </FlexBetween>
-                    </Box>
+      {/*first row*/}
+      <FlexBetween
+        gap="0.4rem"
+        pb="1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap=".9rem">
+          <UserImage image={picturePath} />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}> {friends.length} friends</Typography>
+          </Box>
+          <ManageAccountsOutlined />
         </FlexBetween>
+        <Divider />
+        <Box p=".9rem 0">
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.4rem">
+            <LocationOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{location}</Typography>
+          </Box>
+          <Box display="flex" alignItems="center" gap="1rem">
+            <WorkOutlineOutlined fonstize="large" sx={{ color: main }} />
+            <Typography color={medium}>{occupation}</Typography>
+          </Box>
+        </Box>
+        {/*third row */}
+        <Box p="1rem 0">
+          <FlexBetween mb="0.5rem">
+            <Typography color={medium}>Who's checking you out?</Typography>
+            <Typography color={main} fontWeight="500">
+              {viewedProfile}
+            </Typography>
+          </FlexBetween>
+          <FlexBetween mb="0.5rem">
+            <Typography color={medium}>Content Reactions</Typography>
+            <Typography color={main} fontWeight="500">
+              {impressions}
+            </Typography>
+          </FlexBetween>
+        </Box>
+        <Box p="0.9rem 0">
+          <Typography fonstize="1rem" color={main} fontWeight="500" mb="1rem">
+            Community Profiles
+          </Typography>
+          <FlexBetween gap="1rem" mb="0.5rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/twitter.png" alt="twitter" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Twitter
+                </Typography>
+                <Typography color={medium}>Your Network</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+
+          <Typography fonstize="1rem" color={main} fontWeight="500" mb="1rem">
+            Community Profiles
+          </Typography>
+          <FlexBetween gap="1rem" mb="0.5rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/linkedIn.png" alt="linkedIn" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  linkedIn
+                </Typography>
+                <Typography color={medium}>Network App</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+        </Box>
+      </FlexBetween>
     </WidgetWrapper>
-  )
+  );
 };
